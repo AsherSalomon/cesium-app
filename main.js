d7cfdf2835df2d414dca86496aea6cd6dbb3d014
AsherSalomon <AsherSalomon@gmail.com>
2022-07-08 08:30:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,12 @@
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyZmZjMzQzNi01MGI3LTRiY2ItODE3ZC00OGM3ZjBkZjQxNzUiLCJpZCI6MTAwNDY2LCJpYXQiOjE2NTcyNDAzODl9.ij6tW00jwNgBeDuzMgzMRzS82kQLKucEyLgPhQQs3a4';
 
-// // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
-// const viewer = new Cesium.Viewer('cesiumContainer', {
-//   terrainProvider: Cesium.createWorldTerrain()
-// });
-// // Add Cesium OSM Buildings, a global 3D buildings layer.
-// const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings());
-// // Fly the camera to San Francisco at the given longitude, latitude, and height.
-// viewer.camera.flyTo({
-//   destination : Cesium.Cartesian3.fromDegrees(-71.30325 + 0.003, 44.2705, 1916.7),
-//   orientation : {
-//     heading : Cesium.Math.toRadians(270),
-//     pitch : Cesium.Math.toRadians(0),
-//   }
-// });
-//
-// viewer.homeButton.destroy();
-
-const viewer = new Cesium.CesiumWidget('cesiumContainer', {
+// Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
+const viewer = new Cesium.Viewer('cesiumContainer', {
   terrainProvider: Cesium.createWorldTerrain()
 });
-
+// Add Cesium OSM Buildings, a global 3D buildings layer.
 const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings());
-
+// Fly the camera to San Francisco at the given longitude, latitude, and height.
 viewer.camera.flyTo({
   destination : Cesium.Cartesian3.fromDegrees(-71.30325 + 0.003, 44.2705, 1916.7),
   orientation : {
@@ -31,6 +15,21 @@ viewer.camera.flyTo({
   }
 });
 
+viewer.homeButton.destroy();
+
+// const viewer = new Cesium.CesiumWidget('cesiumContainer', {
+//   terrainProvider: Cesium.createWorldTerrain()
+// });
+//
+// const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings());
+//
+// viewer.camera.flyTo({
+//   destination : Cesium.Cartesian3.fromDegrees(-71.30325 + 0.003, 44.2705, 1916.7),
+//   orientation : {
+//     heading : Cesium.Math.toRadians(270),
+//     pitch : Cesium.Math.toRadians(0),
+//   }
+// });
 
 function createModel(url, height) {
   viewer.entities.removeAll();
@@ -62,9 +61,7 @@ function createModel(url, height) {
   viewer.trackedEntity = entity;
 }
 
-setTimeout( function () {
-  createModel(
-    "1984_Ford_F350.glb",
-    1916.7
-  );
-} );
+createModel(
+  "1984_Ford_F350.glb",
+  1916.7
+);
