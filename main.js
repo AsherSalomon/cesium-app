146a2ebbdc790b445f82f82f0e03df5e8f12a61f
AsherSalomon <AsherSalomon@gmail.com>
2022-07-07 21:01:45 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyZmZjMzQzNi01MGI3LTRiY2ItODE3ZC00OGM3ZjBkZjQxNzUiLCJpZCI6MTAwNDY2LCJpYXQiOjE2NTcyNDAzODl9.ij6tW00jwNgBeDuzMgzMRzS82kQLKucEyLgPhQQs3a4';
 
-// Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
-const viewer = new Cesium.Viewer('cesiumContainer', {
+// // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
+// const viewer = new Cesium.Viewer('cesiumContainer', {
+//   terrainProvider: Cesium.createWorldTerrain()
+// });
+// // Add Cesium OSM Buildings, a global 3D buildings layer.
+// const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings());
+// // Fly the camera to San Francisco at the given longitude, latitude, and height.
+// viewer.camera.flyTo({
+//   destination : Cesium.Cartesian3.fromDegrees(-71.30325 + 0.003, 44.2705, 1916.7),
+//   orientation : {
+//     heading : Cesium.Math.toRadians(270),
+//     pitch : Cesium.Math.toRadians(0),
+//   }
+// });
+//
+// viewer.homeButton.destroy();
+
+const widget = new Cesium.CesiumWidget('cesiumContainer', {
   terrainProvider: Cesium.createWorldTerrain()
 });
-// Add Cesium OSM Buildings, a global 3D buildings layer.
-const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings());
-// Fly the camera to San Francisco at the given longitude, latitude, and height.
-viewer.camera.flyTo({
-  destination : Cesium.Cartesian3.fromDegrees(-71.30325 + 0.003, 44.2705, 1916.7),
-  orientation : {
-    heading : Cesium.Math.toRadians(270),
-    pitch : Cesium.Math.toRadians(0),
-  }
-});
-
-viewer.homeButton.destroy();
