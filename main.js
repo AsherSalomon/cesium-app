3d38a55fd52f1ca783ebf51e506ee4c41f31554a
AsherSalomon <AsherSalomon@gmail.com>
2022-07-09 08:45:49 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 import * as cesiumStuff from './cesium-stuff.js';
 import * as controls from './controls.js';
+
+loop();
+
+function loop() {
+  console.log('hello');
+  window.requestAnimationFrame(loop);
+}
