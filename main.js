df1962aadb1747aa2f714b611e077a9d233ea585
AsherSalomon <AsherSalomon@gmail.com>
2022-07-08 22:27:11 -0400

    Update main.js

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ viewer.scene.globe.depthTestAgainstTerrain = true;
 
 // Fly the camera to Mt Washington at the given longitude, latitude, and height.
 viewer.camera.flyTo({
-  destination : Cesium.Cartesian3.fromDegrees(-71.30325 + 0.003, 44.2705, 1916.7 + 30),
+  destination : Cesium.Cartesian3.fromDegrees(-71.30325 + 0.003, 44.2705, 1916.7 + 35),
   orientation : {
     heading : Cesium.Math.toRadians(270),
     pitch : Cesium.Math.toRadians(-15),
