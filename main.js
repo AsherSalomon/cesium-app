4f864a68a3fa9678bf1eb80d9cd9340cfd363d8e
AsherSalomon <AsherSalomon@gmail.com>
2022-07-09 22:22:26 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 import * as cesiumStuff from './cesium-stuff.js';
 import * as controls from './controls.js';
 
+Ammo().then( function ( AmmoLib ) {
+	Ammo = AmmoLib;
+	init();
+} );
+
+function init() {
+  window.requestAnimationFrame( step );
+}
+
 // let start, previousTimeStamp;
 function step( timestamp ) {
   // if ( start === undefined ) { start = timestamp; }
   // const elapsed = timestamp - start;
-
   controls.update();
-
   // previousTimeStamp = timestamp;
   window.requestAnimationFrame( step );
 }
-
-window.requestAnimationFrame( step );
