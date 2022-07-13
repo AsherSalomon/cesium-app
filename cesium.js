22ebcf781607dd102485c10f6abf1fad8aa23457
AsherSalomon <AsherSalomon@gmail.com>
2022-07-13 07:52:40 -0400

    Update cesium.js

--- cesium.js
+++ cesium.js
@@ -89,10 +89,10 @@ export function init( newTruck ) {
 
   truck.now = function() { return viewer.clock.currentTime; }
 
-  let myCart = new Cesium.Cartographic(-71.30325, 44.2705, 1916.7);
   viewer.scene.globe.terrainProvider.readyPromise.then(function() {
-    console.log(viewer.scene.globe.terrainProvider.tilingScheme.projection);
-    console.log('hi');
+    let myProj = viewer.scene.globe.terrainProvider.tilingScheme.projection;
+    let myCart = new Cesium.Cartographic(-71.30325, 44.2705, 1916.7);
+    console.log(myProj.project(myCart));
   });
 
 }
