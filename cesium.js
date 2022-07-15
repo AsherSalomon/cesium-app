d0ac7400e93e803581fb3f342ed95a3b71166fa9
AsherSalomon <AsherSalomon@gmail.com>
2022-07-15 12:22:36 -0400

    Update cesium.js

--- cesium.js
+++ cesium.js
@@ -1,38 +1,39 @@
 
-Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyZmZjMzQzNi01MGI3LTRiY2ItODE3ZC00OGM3ZjBkZjQxNzUiLCJpZCI6MTAwNDY2LCJpYXQiOjE2NTcyNDAzODl9.ij6tW00jwNgBeDuzMgzMRzS82kQLKucEyLgPhQQs3a4';
-
-const viewer = new Cesium.Viewer('cesiumContainer', {
-  terrainProvider: Cesium.createWorldTerrain({
-    requestWaterMask: true,
-    requestVertexNormals: true,
-  }),
-  geocoder: false,
-  homeButton: false,
-  sceneModePicker: false,
-  baseLayerPicker: false,
-  navigationHelpButton: false,
-  animation: false,
-  timeline: false,
-  fullscreenButton: false,
-});
-
-viewer.clock.clockStep = Cesium.ClockStep.SYSTEM_CLOCK;
-viewer.scene.globe.enableLighting = true;
-viewer.shadows = true;
-viewer.scene.globe.depthTestAgainstTerrain = true;
-viewer.scene.moon = new Cesium.Moon();
-
-viewer.scene.primitives.add(Cesium.createOsmBuildings());
-
-viewer.camera.flyTo({
-  destination : Cesium.Cartesian3.fromDegrees(-71.30325 + 0.003, 44.2705, 1916.7 + 35),
-  orientation : {
-    heading : Cesium.Math.toRadians(270),
-    pitch : Cesium.Math.toRadians(-15),
-  }
-});
+let viewer;
 
 export function init() {
+  Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyZmZjMzQzNi01MGI3LTRiY2ItODE3ZC00OGM3ZjBkZjQxNzUiLCJpZCI6MTAwNDY2LCJpYXQiOjE2NTcyNDAzODl9.ij6tW00jwNgBeDuzMgzMRzS82kQLKucEyLgPhQQs3a4';
+
+  viewer = new Cesium.Viewer('cesiumContainer', {
+    terrainProvider: Cesium.createWorldTerrain({
+      requestWaterMask: true,
+      requestVertexNormals: true,
+    }),
+    geocoder: false,
+    homeButton: false,
+    sceneModePicker: false,
+    baseLayerPicker: false,
+    navigationHelpButton: false,
+    animation: false,
+    timeline: false,
+    fullscreenButton: false,
+  });
+
+  viewer.clock.clockStep = Cesium.ClockStep.SYSTEM_CLOCK;
+  viewer.scene.globe.enableLighting = true;
+  viewer.shadows = true;
+  viewer.scene.globe.depthTestAgainstTerrain = true;
+  viewer.scene.moon = new Cesium.Moon();
+
+  viewer.scene.primitives.add(Cesium.createOsmBuildings());
+
+  viewer.camera.flyTo({
+    destination : Cesium.Cartesian3.fromDegrees(-71.30325 + 0.003, 44.2705, 1916.7 + 35),
+    orientation : {
+      heading : Cesium.Math.toRadians(270),
+      pitch : Cesium.Math.toRadians(-15),
+    }
+  });
 }
 
 export function update() {
