aeee6597f27d8defb79c1cc1e11b7a76050490ee
AsherSalomon <AsherSalomon@gmail.com>
2022-07-15 12:07:50 -0400

    Update controls.js

--- controls.js
+++ controls.js
@@ -1,35 +1,31 @@
-export default class Controls {
-  constructor() {
-    this.forward = false;
-    this.backward = false;
-    this.left = false;
-    this.right = false;
-    this.up = false;
-    this.down = false;
+export var forward = false;
+export var backward = false;
+export var left = false;
+export var right = false;
+export var up = false;
+export var down = false;
 
-    function onKeyDown( event ) {
-      switch ( event.keyCode ) {
-        case 87: this.forward = true; break; // w
-        case 65: this.left = true; break; // a
-        case 83: this.backward = true; break; // s
-        case 68: this.right = true; break; // d
-        case 32: this.up = true; break; // space
-        case 16: this.down = true; break; // shift
-      }
-    };
-
-    function onKeyUp( event ) {
-      switch ( event.keyCode ) {
-        case 87: this.forward = false; break; // w
-        case 65: this.left = false; break; // a
-        case 83: this.backward = false; break; // s
-        case 68: this.right = false; break; // d
-        case 32: this.up = false; break; // space
-        case 16: this.down = false; break; // shift
-      }
-    };
-
-    document.addEventListener( 'keydown', onKeyDown, false );
-    document.addEventListener( 'keyup', onKeyUp, false );
+function onKeyDown( event ) {
+  switch ( event.keyCode ) {
+    case 87: forward = true; break; // w
+    case 65: left = true; break; // a
+    case 83: backward = true; break; // s
+    case 68: right = true; break; // d
+    case 32: up = true; break; // space
+    case 16: down = true; break; // shift
   }
-}
+};
+
+function onKeyUp( event ) {
+  switch ( event.keyCode ) {
+    case 87: forward = false; break; // w
+    case 65: left = false; break; // a
+    case 83: backward = false; break; // s
+    case 68: right = false; break; // d
+    case 32: up = false; break; // space
+    case 16: down = false; break; // shift
+  }
+};
+
+document.addEventListener( 'keydown', onKeyDown, false );
+document.addEventListener( 'keyup', onKeyUp, false );
