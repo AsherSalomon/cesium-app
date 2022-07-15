5bdee9d1495a42fc764e33deaf247cd548b6fa50
AsherSalomon <AsherSalomon@gmail.com>
2022-07-15 14:02:19 -0400

    Update cesium.js

--- cesium.js
+++ cesium.js
@@ -116,12 +116,7 @@ function updateTileList(newTileList) {
     }
     if (oldTileInNewList == false) {
       // console.log('remove', tileList[i]._rectangle);
-      // viewer.entities.remove({
-      //   rectangle: {
-      //     coordinates: tileList[i]._rectangle,
-      //   },
-      //   material: Cesium.Color.GREEN.withAlpha(0.5),
-      // });
+      viewer.entities.remove(tileList[i].entity);
       tileList.splice(i, 1);
     }
   }
@@ -136,13 +131,14 @@ function updateTileList(newTileList) {
     }
     if (newTileInOldList == false) {
       // console.log('add', newTileList[i]._rectangle);
-      viewer.entities.add({
+      let tileToPush = newTileList[i];
+      tileToPush.entity = viewer.entities.add({
         rectangle: {
           coordinates: newTileList[i]._rectangle,
         },
         material: Cesium.Color.GREEN.withAlpha(0.5),
       });
-      tileList.push(newTileList[i]);
+      tileList.push(tileToPush);
     }
   }
 }
