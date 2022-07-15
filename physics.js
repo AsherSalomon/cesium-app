c763bcfc14424922c4db259425a6f87566e40c31
AsherSalomon <AsherSalomon@gmail.com>
2022-07-15 13:31:23 -0400

    Update physics.js

--- physics.js
+++ physics.js
@@ -2,7 +2,7 @@ import * as controls from './controls.js';
 
 let truckEntity;
 
-export function init( newTruck ) {
+export function init(newTruck) {
   truckEntity = newTruck;
 }
 
@@ -11,9 +11,9 @@ export function update() {
   let upDown = controls.down - controls.up;
   let forwardBackward = controls.forward - controls.backward;
 
-  let position = truckEntity.position.getValue( truckEntity.now() );
+  let position = truckEntity.position.getValue(truckEntity.now());
   position.x += leftRight;
   position.y += upDown;
   position.z += forwardBackward;
-  truckEntity.position = new Cesium.ConstantPositionProperty( position );
+  truckEntity.position = new Cesium.ConstantPositionProperty(position);
 }
