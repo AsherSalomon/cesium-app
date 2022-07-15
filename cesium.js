c1d8b5c6af7f49552dd906d3e1a378efe9509eed
AsherSalomon <AsherSalomon@gmail.com>
2022-07-15 12:53:48 -0400

    Update cesium.js

--- cesium.js
+++ cesium.js
@@ -53,8 +53,8 @@ export function update() {
     let cartographic = ellipsoid.cartesianToCartographic(position);
     let level = provider.availability.computeMaximumLevelAtPosition(cartographic);
     let cartesian2 = provider.tilingScheme.positionToTileXY(cartographic, level);
-    selectedTile[cartesian2] = cartesian2;
-    selectedTile[level] = cartesian2;
+    selectedTile['cartesian2'] = cartesian2;
+    selectedTile['level'] = cartesian2;
     console.log(selectedTile);
   }
 }
