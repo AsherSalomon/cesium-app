9af1fc51f568349d6119d5f8fa06610809e93a42
AsherSalomon <AsherSalomon@gmail.com>
2022-07-16 22:16:51 -0400

    Update physics.js

--- physics.js
+++ physics.js
@@ -281,7 +281,7 @@ function createVehicle(pos, quat) {
     // quaternion.w = q.w();
     const quaternion = new Cesium.Quaternion(q.x(), q.y(), q.z(), q.w());
     truckEntity.orientation = new Cesium.ConstantPositionProperty(quaternion);
-    console.log(quaternion.magnitude);
+    console.log(Cesium.Quaternion.magnitude(quaternion));
   }
 
 	syncList.push(sync);
