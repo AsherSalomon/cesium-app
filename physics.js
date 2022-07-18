4752ce4d834dc7b9a4ef899ae2110ba03191d565
AsherSalomon <AsherSalomon@gmail.com>
2022-07-17 22:11:28 -0400

    Update physics.js

--- physics.js
+++ physics.js
@@ -171,7 +171,8 @@ function createVehicle(pos, quat) {
 	transform.setIdentity();
 	transform.setOrigin(new Ammo.btVector3(pos.x, pos.y, pos.z));
   Cesium.Quaternion.inverse(quat, quat);
-	transform.setRotation(new Ammo.btQuaternion(quat.x, quat.y, quat.z, quat.w));
+	// transform.setRotation(new Ammo.btQuaternion(quat.x, quat.y, quat.z, quat.w));
+	transform.setRotation(new Ammo.btQuaternion(0, 0, 0, 1));
 
 	const motionState = new Ammo.btDefaultMotionState(transform);
 	const localInertia = new Ammo.btVector3(0, 0, 0);
