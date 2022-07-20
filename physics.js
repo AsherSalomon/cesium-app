b6c5bf5efda5b0febbcdc369a464eeaa4784d2fa
AsherSalomon <AsherSalomon@gmail.com>
2022-07-19 21:03:51 -0400

    Update physics.js

--- physics.js
+++ physics.js
@@ -147,7 +147,7 @@ function createVehicle(pos, quat) {
 	const wheelHalfTrackBack = 0.8;
 	const wheelAxisHeightBack = 0.3;
 
-	const wheelAxisFrontPosition = 1.42;
+	const wheelAxisFrontPosition = 1.43;
 	const wheelHalfTrackFront = 0.8;
 	const wheelAxisHeightFront = 0.3;
 	const wheelRadiusFront = 0.432;
