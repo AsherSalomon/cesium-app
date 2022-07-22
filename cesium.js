80d8ef748664b8f10182ed85cafa00407bd5e1c1
AsherSalomon <AsherSalomon@gmail.com>
2022-07-21 23:07:43 -0400

    Update cesium.js

--- cesium.js
+++ cesium.js
@@ -100,9 +100,6 @@ export function update() {
   // Cesium.ScreenSpaceCameraController.adjustHeightForTerrain(
   //   viewer.scene.screenSpaceCameraController
   // );
-  const temp = viewer.trackedEntity;
-  viewer.trackedEntity = undefined;
-  viewer.trackedEntity = temp;
 
 }
 
