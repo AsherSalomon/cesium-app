efdadaa661de603d6e6f6172d3011a3168bacf81
AsherSalomon <AsherSalomon@gmail.com>
2022-07-24 17:33:00 -0400

    Update physics.js

--- physics.js
+++ physics.js
@@ -149,8 +149,8 @@ function createVehicle(pos, quat) {
 
 	const friction = 1000;
 	const suspensionStiffness = 20.0;
-	const suspensionDamping = 2.3;
-	const suspensionCompression = 4.4;
+	const suspensionDamping = 5;//2.3;
+	const suspensionCompression = 9;//4.4;
 	const suspensionRestLength = 0.8;
 	const rollInfluence = 0.2;
 
