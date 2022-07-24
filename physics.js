adc45d429a4b9ef44afe4d279a646862ab90cf06
AsherSalomon <AsherSalomon@gmail.com>
2022-07-24 17:24:51 -0400

    Update physics.js

--- physics.js
+++ physics.js
@@ -217,7 +217,7 @@ function createVehicle(pos, quat) {
 		wheelInfo.set_m_rollInfluence(rollInfluence);
 
     wheelInfo.set_m_maxSuspensionForce(1000000); // improves underworld problem
-    wheelInfo.set_m_wheelsDampingRelaxation(0);
+    wheelInfo.set_m_wheelsDampingCompression(0);
     if (index == 0) {
       console.log(wheelInfo.m_suspensionRestLength);
       console.log(wheelInfo.m_maxSuspensionTravelCm);
