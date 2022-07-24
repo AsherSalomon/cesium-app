f4aa529c83729f72e43e8edbd3cb2f1b89217433
AsherSalomon <AsherSalomon@gmail.com>
2022-07-23 22:47:52 -0400

    Update physics.js

--- physics.js
+++ physics.js
@@ -357,7 +357,7 @@ function createVehicle(pos, quat) {
       const cartographic = Cesium.Cartographic.fromCartesian(position, ellipsoid);
       const bodyHeight = cartographic.height;
       if (bodyHeight < terrainHeight) {
-        const terrainSpringRate = 100;
+        const terrainSpringRate = 1000;
         const terrainForce = (terrainHeight - bodyHeight) * terrainSpringRate;
         Cesium.Cartesian3.normalize(position, position);
         Cesium.Cartesian3.multiplyByScalar(position, terrainForce, position);
