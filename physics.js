e35f0cb7b420ab58c3d9d5eb64ea7077a0a5bdbc
AsherSalomon <AsherSalomon@gmail.com>
2022-07-26 23:44:30 -0400

    Update physics.js

--- physics.js
+++ physics.js
@@ -135,18 +135,18 @@ function createVehicle(pos, quat) {
 	const maxBreakingForce = 236; // 50;
 
 	// Chassis
-	// const geometry = new Ammo.btBoxShape(new Ammo.btVector3(chassisWidth * .5, chassisHeight * .5, chassisLength * .5));
+	const geometry = new Ammo.btBoxShape(new Ammo.btVector3(chassisWidth * .5, chassisHeight * .5, chassisLength * .5));
   // const geometry = new Ammo.btSphereShape(chassisHeight);
   // const geometry = new Ammo.btCapsuleShapeZ(chassisHeight * .5, chassisLength);
-  const ch5 = chassisHeight * .5;
-  const geometry = new Ammo.btMultiSphereShape(
-    [new Ammo.btVector3(chassisWidth * .5 - ch5, 0, chassisLength * .5 - ch5),
-    new Ammo.btVector3(chassisWidth * .5 - ch5, 0, -(chassisLength * .5 - ch5)),
-    new Ammo.btVector3(-(chassisWidth * .5) - ch5, 0, chassisLength * .5 - ch5),
-    new Ammo.btVector3(-(chassisWidth * .5) - ch5, 0, -(chassisLength * .5 - ch5))],
-    [chassisHeight * .5, chassisHeight * .5, chassisHeight * .5, chassisHeight * .5],
-    4
-  );
+  // const ch5 = chassisHeight * .5;
+  // const geometry = new Ammo.btMultiSphereShape(
+  //   [new Ammo.btVector3(chassisWidth * .5 - ch5, 0, chassisLength * .5 - ch5),
+  //   new Ammo.btVector3(chassisWidth * .5 - ch5, 0, -(chassisLength * .5 - ch5)),
+  //   new Ammo.btVector3(-(chassisWidth * .5) - ch5, 0, chassisLength * .5 - ch5),
+  //   new Ammo.btVector3(-(chassisWidth * .5) - ch5, 0, -(chassisLength * .5 - ch5))],
+  //   [chassisHeight * .5, chassisHeight * .5, chassisHeight * .5, chassisHeight * .5],
+  //   4
+  // );
 
 	const localInertia = new Ammo.btVector3(0, 0, 0);
 	geometry.calculateLocalInertia(massVehicle, localInertia);
