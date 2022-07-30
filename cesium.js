5aebd6958273233ca88b15606b5fbc02ca4801b4
AsherSalomon <AsherSalomon@gmail.com>
2022-07-29 20:20:13 -0400

    Update cesium.js

--- cesium.js
+++ cesium.js
@@ -122,6 +122,7 @@ export function update() {
     // console.log(document.getElementsByClassName("cesium-viewer")[0]);
     const element = document.getElementsByClassName("cesium-widget")[0];
     if (element != null && once) {
+      console.log('hi');
       element.addEventListener('drag', function() {
         console.log('drag');
         once = false;
