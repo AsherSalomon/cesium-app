8f3fd6daf65fd68bbb9fd2f3e0bf07e4b23a8d37
AsherSalomon <AsherSalomon@gmail.com>
2022-07-30 09:31:19 -0400

    Update physics.js

--- physics.js
+++ physics.js
@@ -408,7 +408,7 @@ function createVehicle(pos, quat) {
 
 }
 
-class DestroyableTerrainA {
+class DestroyableTerrain {
   constructor(positions, indices, skirtHeight) {
     // https://stackoverflow.com/questions/59665854/ammo-js-custom-mesh-collision-with-sphere
     this.mesh = new Ammo.btTriangleMesh(false, false);
@@ -456,7 +456,7 @@ class DestroyableTerrainA {
 
 }
 
-class DestroyableTerrain {
+class DestroyableTerrainB {
   constructor(positions, indices, skirtHeight) {
     this.shapes = new Array(indices.length / 3);
     this.vertices = new Array(positions.length);
