ec29466abecfbe62859c7dae6db87e20a35e7a46
AsherSalomon <AsherSalomon@gmail.com>
2022-07-29 20:13:49 -0400

    Update cesium.js

--- cesium.js
+++ cesium.js
@@ -118,7 +118,8 @@ export function update() {
     Cesium.Cartesian3.cross(viewer.camera.directionWC, vehicleDirection, crossProduct);
     const dotProduct = Cesium.Cartesian3.dot(viewer.camera.upWC, crossProduct);
     viewer.camera.rotateRight(dotProduct * Math.PI / 256);
-    console.log(document.getElementById("cesiumContainer"));
+    // console.log(document.getElementById("cesiumContainer"));
+    console.log(document.getElementsByClassName("cesium-viewer"));
     // document.getElementById("cesium-widget").addEventListener('drag', function() {
     //   console.log('drag');
     // });
