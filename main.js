357d5637b4e4d50af512bef7e8b9aa2bcb3d63b2
AsherSalomon <AsherSalomon@gmail.com>
2022-07-31 22:00:49 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import * as cesium from './cesium.js';
 import * as physics from './physics.js';
 
+let start, previousTimeStamp;
+
 init();
 animate();
 
@@ -9,7 +11,6 @@ function init() {
   physics.init();
 }
 
-let start, previousTimeStamp;
 function animate(timestamp) {
   if (start === undefined) {start = timestamp;}
 	const delta = (timestamp - previousTimeStamp) / 1000;
