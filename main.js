8111fefaa229f28c4b550c959f478e16de3efbe5
AsherSalomon <AsherSalomon@gmail.com>
2022-07-31 21:59:39 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,12 @@
 import * as cesium from './cesium.js';
 import * as physics from './physics.js';
 
-let waitingForPhysicsInit = true;
-
-Ammo().then(function (AmmoLib) {
-	Ammo = AmmoLib;
-	init();
-  animate();
-});
+init();
+animate();
 
 function init() {
   cesium.init();
-	// console.log('waiting...');
-	setTimeout(function() {
-		// console.log('physics.init');
-	  physics.init(cesium.truckEntities, cesium.viewer);
-		cesium.getPhysicsFunctions(physics.createTerrain, physics.removeTerrain);
-		waitingForPhysicsInit = false;
-	}, 1 * 1000);
+  physics.init();
 }
 
 let start, previousTimeStamp;
@@ -27,9 +16,7 @@ function animate(timestamp) {
   previousTimeStamp = timestamp;
 
   cesium.update();
-	if (waitingForPhysicsInit == false) {
-  	physics.update(delta);
-	}
+	physics.update(delta);
 
   window.requestAnimationFrame(animate);
 }
