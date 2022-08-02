457bef26a3e09ab0614b6e857d2a18c5de97e0a4
AsherSalomon <AsherSalomon@gmail.com>
2022-08-01 23:45:07 -0400

    Update physics.js

--- physics.js
+++ physics.js
@@ -6,8 +6,8 @@ export function init(newTruck, newViewer) {
   truckEntities = newTruck;
   viewer = newViewer;
 
-  position = truckEntities[0].position.getValue(truckEntities.now());
-  quaternion = truckEntities[0].orientation.getValue(truckEntities.now());
+  // position = truckEntities[0].position.getValue(truckEntities.now());
+  // quaternion = truckEntities[0].orientation.getValue(truckEntities.now());
 
 }
 
