dc95476aa5d23413c6656e63b701e81a2f52d163
AsherSalomon <AsherSalomon@gmail.com>
2022-08-01 22:38:13 -0400

    Update physics.js

--- physics.js
+++ physics.js
@@ -5,8 +5,18 @@ export function init(newTruck, newViewer) {
   truckEntities = newTruck;
   viewer = newViewer;
 
+  const position = truckEntities[0].position.getValue(truckEntities.now());
+  const quaternion = truckEntities[0].orientation.getValue(truckEntities.now());
+
 }
 
 export function update(delta) {
+
+  for (let i = 0; i < 4; i++) {
+    truckEntities[i + 1].position = position;
+    truckEntities[i + 1].orientation = quaternion;
+  }
+  truckEntities[0].position = position;
+  truckEntities[0].orientation = quaternion;
 
 }
