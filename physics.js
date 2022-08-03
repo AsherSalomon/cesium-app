462fa75aa19ef0279c6edfc9a2549d45a86db3f0
AsherSalomon <AsherSalomon@gmail.com>
2022-08-02 21:24:26 -0400

    Update physics.js

--- physics.js
+++ physics.js
@@ -22,25 +22,34 @@ export function update(delta) {
 
 }
 
-class Physical {
+class PhysicalObject {
   constructor(entity, mass) {
     this.entity = entity;
     // this.position = new Cesium.Cartesian3();
     // this.orientation = new Cesium.Quaternion(0, 0, 0, 1);
 
     this.position = entity.position.getValue(viewer.clock.currentTime);
-    this.orientation = entity.orientation.getValue(viewer.clock.currentTime);
-
     this.velocity = new Cesium.Cartesian3();
-    this.angularVelocity = new Cesium.Cartesian3();
     this.forceSum = new Cesium.Cartesian3();
-    this.torqueSum = new Cesium.Cartesian3();
     this.mass = mass;
+
+    this.orientation = entity.orientation.getValue(viewer.clock.currentTime);
+    this.angularVelocity = new Cesium.Cartesian3();
+    this.torqueSum = new Cesium.Cartesian3();
     this.momentOfInertia = new Cesium.Matrix3();
   }
 
   update() {
+    Cesium.Cartesian3.add(this.position, this.velocity, this.position);
+    Cesium.Cartesian3.divideByScalar(this.forceSum, this.mass, this.forceSum);
+    Cesium.Cartesian3.add(this.velocity, this.forceSum, this.velocity);
     this.entity.position = this.position;
+
+    const rotationAngle = this.angularVelocity.magnitude();
+    const rotationAxis = this.angularVelocity.clone();
+    Cesium.Cartesian3.normalize(rotationAxis, rotationAxis);
+    const rotation = new Cesium.Quaternion.fromAxisAngle(axis, angle, result)
+    Cesium.Quaternion.add(this.orientation, this.angularVelocity, this.orientation);
     this.entity.orientation = this.quaternion;
   }
 
