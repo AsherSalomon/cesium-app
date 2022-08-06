18ac09a91e024fa78fdda357f3c9f4e8f70c10ea
AsherSalomon <AsherSalomon@gmail.com>
2022-08-06 09:59:51 -0400

    Update cesium.js

--- cesium.js
+++ cesium.js
@@ -191,6 +191,7 @@ window.addEventListener('keydown', function(e) {
 
       const matrix4 = new Cesium.Matrix4();
       Cesium.Transforms.eastNorthUpToFixedFrame(truckEntities[0].position._value, Cesium.Ellipsoid.WGS84, matrix4);
+      Cesium.Matrix4.inverse(matrix4, matrix4);
       const position = new Cesium.Cartesian3();
       Cesium.Matrix4.multiplyByPoint(matrix4, truckEntities[0].position._value, position);
       truckEntities[0].viewFrom = position;
