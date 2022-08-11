af0cdfc915edf905df86017501c50f8f96bc0d5c
AsherSalomon <AsherSalomon@gmail.com>
2022-08-11 08:41:48 -0400

    Update physics.js

--- physics.js
+++ physics.js
@@ -77,6 +77,9 @@ export function update(delta) {
     Cesium.Quaternion.fromAxisAngle(Cesium.Cartesian3.UNIT_Y, Math.PI, quatB);
     Cesium.Quaternion.multiply(orient, quatB, orient);
   	tm.setRotation(new Ammo.btQuaternion(orient.x, orient.y, orient.z, orient.w));
+
+    const body = vehicle.getRigidBody();
+    body.setLinearVelocity(new Ammo.btVector3(0, 0, 0));
   }
   if (truckSelected) {
     if (gravityOn) {
